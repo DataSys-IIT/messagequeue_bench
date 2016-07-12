636192cdc4f6b06fa3e5fe3e3b44711361bd08e7
gnu3ra <alexandersballmer@gmail.com>
2016-07-11 21:45:55 -0500

    Sketchy timing system

--- src/graph.hpp
+++ src/graph.hpp
@@ -6,36 +6,67 @@
 
 adjlist<Meme*> queue;
 
-
 int errorcount = 0;
 
 
+  using std::chrono::duration_cast;
+  using std::chrono::nanoseconds;
+    
+ 
+  std::chrono::time_point<std::chrono::system_clock> timestart;
 void fill(int id, int count) {
+  if(id==0) {
+    using std::chrono::duration_cast;
+    using std::chrono::nanoseconds;
+ 
+    std::cout << "Filling single test queue\n"; 
+    timestart = std::chrono::high_resolution_clock::now();
+  }
+
   for(int x=0;x<count;x++) {
     Meme * m = new Meme();
     queue.listAt(0)->push(m);
   }
-}
+  if(id==0) {    
+    using std::chrono::duration_cast;
+    using std::chrono::nanoseconds;
+  
 
-void empty(int id) {
+ 
+    std::cout << "Emptying single test queue\n";
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Finished in " << (double)(end-timestart).count()/1000000000.00 <<  " sec\n";
+
+    timestart = std::chrono::high_resolution_clock::now();
+    
+  }
+
   while(queue.listAt(0)->empty()) {
     Meme * m;
     bool result = queue.listAt(0)->pop(m);
     if(!result)
       errorcount++;
   }
+
+  if(id==0) {
+    using std::chrono::duration_cast;
+    using std::chrono::nanoseconds;
+    
+    typedef std::chrono::high_resolution_clock clock;
+ 
+    auto end = clock::now();
+    std::cout << "Finished in " << (double)(end-timestart).count()/1000000000.00 <<  " sec\n";
+  }
+}
+
+void empty(int id) {
+  
 }
 
 
 void start(int numThreads, int count) {
-  using std::chrono::duration_cast;
-  using std::chrono::nanoseconds;
-
-  typedef std::chrono::high_resolution_clock clock;
-  auto start = clock::now();
-
-  std::vector<std::thread*> threads;
   
+  std::vector<std::thread*> threads;
   for(int i=0;i<numThreads;i++) {
     threads.push_back(new std::thread(&fill, i, count));
   }
