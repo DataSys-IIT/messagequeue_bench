2947405423604610466bc60c909896693a9de792
gnu3ra <alexandersballmer@gmail.com>
2016-07-11 20:55:55 -0500

    Empty function

--- src/graph.hpp
+++ src/graph.hpp
@@ -7,13 +7,27 @@
 adjlist<Meme*> queue;
 
 
+int errorcount = 0;
 
-void goBench(int id) {
 
+void fill(int id, int count) {
+  for(int x=0;x<count;x++) {
+    Meme * m = new Meme();
+    queue.listAt(0)->push(m);
+  }
+}
+
+void empty(int id) {
+  while(queue.listAt(0)->empty()) {
+    Meme * m;
+    bool result = queue.listAt(0)->pop(m);
+    if(!result)
+      errorcount++;
+  }
 }
 
 
-void start(int numThreads) {
+void start(int numThreads, int count) {
   using std::chrono::duration_cast;
   using std::chrono::nanoseconds;
 
@@ -23,7 +37,7 @@ void start(int numThreads) {
   std::vector<std::thread*> threads;
   
   for(int i=0;i<numThreads;i++) {
-    threads.push_back(new std::thread(&goBench, i));
+    threads.push_back(new std::thread(&fill, i, count));
   }
 
 }
