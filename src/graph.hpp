da18c5bd6d2a2eabdda3554521d8264da720528e
gnu3ra <alexandersballmer@gmail.com>
2016-07-11 20:29:27 -0500

    Simple threading

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,6 +1,29 @@
 #include "adjlist.hpp"
+#include <chrono>
+#include <thread>
+#include <iostream>
 
 
 adjlist<Meme*> queue;
 
 
+
+void goBench(int id) {
+
+}
+
+
+void start(int numThreads) {
+  using std::chrono::duration_cast;
+  using std::chrono::nanoseconds;
+
+  typedef std::chrono::high_resolution_clock clock;
+  auto start = clock::now();
+
+  std::vector<std::thread*> threads;
+  
+  for(int i=0;i<numThreads;i++) {
+    threads.push_back(new std::thread(&goBench, i));
+  }
+
+}
